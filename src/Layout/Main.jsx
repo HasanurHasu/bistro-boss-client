e0bbe26915a6c1c7cd1a1b89bbfada94e49f821b
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-11-17 22:20:02 +0600

    install slider

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Pages/Navbar/Navbar';
+import Footer from '../Home/Footer';
 
 const Main = () => {
     return (
         <>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </>
     );
 };
